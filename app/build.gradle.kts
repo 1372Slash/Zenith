35d3e26e30b301438701beac6b7def26b67f0d97
/slash <133662518+1372Slash@users.noreply.github.com>
2026-05-08 16:23:10 +0800

    chore(version) : bump versionName to 1.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 4
-        versionName = "1.4.2"
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
