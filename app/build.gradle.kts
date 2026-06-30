371588a5591353b60be109c2d6cd13e7f201a330
/slash <133662518+1372Slash@users.noreply.github.com>
2026-06-30 17:44:08 +0800

    chore(version) : bump version code to 11 and version name to 1.5.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.etrisad.zenith"
         minSdk = 30
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.5.6"
+        versionCode = 11
+        versionName = "1.5.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
